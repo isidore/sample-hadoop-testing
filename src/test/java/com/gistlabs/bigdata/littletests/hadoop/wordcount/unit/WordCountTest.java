68b0b00de9b5908e0114bdc312842652f32223c2
Llewellyn Falco <isidore@setgame.com>
2012-09-18 16:25:23 -0700

    Inlined results for easy reading

--- src/test/java/com/gistlabs/bigdata/littletests/hadoop/wordcount/unit/WordCountTest.java
+++ src/test/java/com/gistlabs/bigdata/littletests/hadoop/wordcount/unit/WordCountTest.java
@@ -8,21 +8,58 @@ import com.gistlabs.bigdata.littletests.hadoop.wordcount.WordCountMapper;
 import com.gistlabs.bigdata.littletests.hadoop.wordcount.WordCountReducer;
 
 @UseReporter(DiffReporter.class)
-public class WordCountTest// extends AbstractWordCount
+public class WordCountTest
 {
   @Test
   public void testMapper() throws Exception
   {
     HadoopApprovals.verifyMapping(new WordCountMapper(), 0, "cat cat dog");
   }
+  /*
+   * **************** Results For Mapper Inlined ******************
+   * [cat cat dog]
+   * 
+   * -> maps via WordCountMapper to ->
+   * 
+   * (cat, 1)
+   * (cat, 1)
+   * (dog, 1)
+   * **************** Results Inlined ******************
+   */
   @Test
   public void testReducer() throws Exception
   {
     HadoopApprovals.verifyReducer(new WordCountReducer(), "Smiles", 3, 4);
   }
+  /*
+   * **************** Results For Reducer Inlined ******************
+   * (Smiles, [3, 4])
+   * 
+   * -> reduces via WordCountReducer to ->
+   * 
+   * (Smiles, 7)
+   * **************** Results Inlined ******************
+   */
   @Test
   public void testMapReduce() throws Exception
   {
     HadoopApprovals.verifyMapReduce(new WordCountMapper(), new WordCountReducer(), 0, "cat cat dog");
   }
+  /*
+   * **************** Results For MapReduce Inlined ******************
+   * [cat cat dog]
+   * 
+   * -> maps via WordCountMapper to ->
+   * 
+   * (cat, 1)
+   * (cat, 1)
+   * (dog, 1)
+   * 
+   * 
+   * -> reduces via WordCountReducer to ->
+   * 
+   * (cat, 2)
+   * (dog, 1)
+   * **************** Results Inlined ******************
+   */
 }
